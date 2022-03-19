cfabe0c81df1cfead54dbb2502a57b3c73638eb8
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-03-19 01:21:43 -0300

    Skills

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Skills from "./components/Skills";
 import "./styles.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <Home />
+      <Skills />
     
     </> 
   );
