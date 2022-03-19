1bac71700560a184c7656c0525bc71940e384d11
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-03-18 22:45:36 -0300

    Home fix

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import "./styles.css"
+import img  from "../../assets/euHome.svg"
+import img2  from "../../assets/Titik-titik.png"
 
 export default function Home() {
   return (
-    <section className='home'>
-        index
+    <section className='home container'>
+        <div className='container-img'>
+          <img className='img-efeito1' src={img2} alt="" />
+          <img className='img-fluid img-eu' src={img} alt="" />
+          <img className='img-efeito2'  src={img2} alt="" />
+        </div>
+      <div className='container-description'>
+          <h2>Um pouco sobre mim</h2>
+          <p> procuro dar sempre meu melhor para superar as dificuldades e buscar as melhores soluções. Estou bastante empolgado com o futuro da tecnologia
+            e o quanto ela pode tornar problemas complexos  e chatos em problemas simples e mais prazerosos de se realizar. 
+            Prentendo aprender  cada vez mais para fazer parte desse futuro tecnologico e tudo que vai vir com ele,
+            Estou gostando bastante de java e estou focando bastante tempo para melhorar cada vez mais.</p>
+            <a href="#">Download CV</a>
+      </div>
+    
     </section>
   )
 }
